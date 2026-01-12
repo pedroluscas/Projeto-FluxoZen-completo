2fe8b3de4930fa622441e453c55947aced06f49e
pedroluscas <lucassouzatm2@gmail.com>
2026-01-12 02:14:04 -0300

    chore: enable environment variables for supabase

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zkafxqqwjeuiaypfntss.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprYWZ4cXF3amV1aWF5cGZudHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNzM1MDEsImV4cCI6MjA4Mzc0OTUwMX0.pu25yTlaU0y9TKEP-axSs5_FdqhMSHyUAb48pgeuUTc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://zkafxqqwjeuiaypfntss.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprYWZ4cXF3amV1aWF5cGZudHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNzM1MDEsImV4cCI6MjA4Mzc0OTUwMX0.pu25yTlaU0y9TKEP-axSs5_FdqhMSHyUAb48pgeuUTc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
